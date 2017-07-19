d5c802b8f7094abe57c8d95f484da6ecec917706
rodowi <rwilhelmy@gmail.com>
2017-07-18 23:34:50 -0700

    Load scoring matrices: similarity, BLOSUM62, PAM250

--- test/data.test.js
+++ test/data.test.js
@@ -1,9 +1,12 @@
 var test = require('tape')
 var data = require('../lib/data')
 
+let v = 'GCATGCU'
+let w = 'GATTACA'
+let BLOSUM62
+let PAM250
+
 test('createTable', function (t) {
-  let v = 'GCATGCU'
-  let w = 'GATTACA'
   let table = data.createTable(v, w)
   t.equal(table.length, w.length + 1)
   t.equal(table[0].length, v.length + 1)
@@ -25,9 +28,43 @@ test('identityMatrix', function (t) {
   t.end()
 })
 
+test('loads BLOSUM62', function (t) {
+  data.scoringMatrix(v, w, 'BLOSUM62', (err, matrix) => {
+    t.ifError(err)
+    BLOSUM62 = matrix
+    t.end()
+  })
+})
+
+test('loads PAM250', function (t) {
+  data.scoringMatrix(v, w, 'PAM250', (err, matrix) => {
+    t.ifError(err)
+    PAM250 = matrix
+    t.end()
+  })
+})
+
+test('scoringMatrix', function (t) {
+  let matrix = data.scoringMatrix(v, w)
+  t.equal(matrix['A']['A'], 1, 'defaults to similarity scoring')
+  t.equal(matrix['A']['U'], 0)
+  t.equal(matrix['C']['G'], 0)
+  t.equal(matrix['G']['G'], 1)
+
+  t.equal(BLOSUM62['A']['A'], +4)
+  t.equal(BLOSUM62['A']['U'], undefined)
+  t.equal(BLOSUM62['C']['G'], -3)
+  t.equal(BLOSUM62['G']['G'], +6)
+
+  t.equal(PAM250['A']['A'], +2)
+  t.equal(PAM250['A']['U'], undefined)
+  t.equal(PAM250['C']['G'], -3)
+  t.equal(PAM250['G']['G'], +5)
+
+  t.end()
+})
+
 test('vocabulary', function (t) {
-  let v = 'GCATGCU'
-  let w = 'GATTACA'
   t.deepEqual(data.vocabulary(v + w), ['A', 'C', 'G', 'T', 'U'])
   t.end()
 })
