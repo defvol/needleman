2f0310b32424f49bcdf3e87f0a1d1e380f3f0353
rodowi <rwilhelmy@gmail.com>
2017-07-19 19:06:39 -0700

    Backtrack arrow matrix to find alignments

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,15 @@
+var data = require('../lib/data')
 var test = require('tape')
 var needleman = require('../lib/index')
 
 test('run', function (t) {
   let v = 'GCATGCU'
   let w = 'GATTACA'
-  let res = needleman.run(v, w)
-  t.equal(res.score, 3)
+  let scores = { match: +1, mismatch: -1 }
+  let scoringMatrix = data.scoringMatrix({ v, w, scores })
+  let res = needleman.run(v, w, { scoringMatrix })
+  t.equal(res.score, 0)
+  t.equal(res.vAligned, 'GCA-TGCU')
+  t.equal(res.wAligned, 'G-ATTACA')
   t.end()
 })
